c2c7d39e59319842082a644a941541f9aa65152a
barendbentvelsenVO <barend.bentvelsen@vanoord.com>
2020-12-03 18:48:11 +0100

    added models file with objects based on SQLAlchemy declarative base class

---
+++ tutorial/models.py
@@ -0,0 +1,47 @@
+# tutorial/models.py
+
+from datetime import datetime
+import secrets
+
+from sqlalchemy.ext.declarative import declarative_base
+
+
+from sqlalchemy import (
+            Column, Unicode, Integer, DateTime, Boolean, relationship
+            )
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class Task(Base):
+    """Tasks for the tutorial To Do list."""
+    id = Column(Integer, primary_key=True)
+    name = Column(Unicode, nullable=False)
+    note = Column(Unicode)
+    creation_date = Column(DateTime, nullable=False)
+    due_date = Column(DateTime)
+    completed = Column(Boolean, default=False)
+    user_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+    user = relationship("user", back_populates="tasks")
+
+    def __init__(self, *args, **kwargs):
+        """On construction, set date of creation."""
+        super().__init__(*args, **kwargs)
+        self.creation_date = datetime.now()
+
+
+class User(Base):
+    """The User object that owns tasks."""
+    id = Column(Integer, primary_key=True)
+    username = Column(Unicode, nullable=False)
+    email = Column(Unicode, nullable=False)
+    password = Column(Unicode, nullable=False)
+    date_joined = Column(DateTime, nullable=False)
+    token = Column(Unicode, nullable=False)
+    tasks = relationship("Task", back_populates="user")
+
+    def __init__(self, *args, **kwargs):
+        """On construction, set date of creation."""
+        super().__init__(*args, **kwargs)
+        self.date_joined = datetime.now()
+        self.token = secrets.token_urlsafe(64)
