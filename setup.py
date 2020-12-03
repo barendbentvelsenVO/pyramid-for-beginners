d41f1e60143d9ba1e3ad5e32c927111bcc8a17b6
barendbentvelsenVO <barend.bentvelsen@vanoord.com>
2020-12-03 18:19:04 +0100

    added basic setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+etup.py
+from setuptools import setup, find_packages
+
+requires = [
+        'pyramid',
+        'paster_pastedeploy',
+        'pyramid-ipython',
+        'waitress'
+        ]
+
+setup(
+        name='pyramid-for-beginners',
+        version='0.0',
+        description='An introduction to start with Pyramid',
+        author='B.J. Bentvelsen',
+        author_email='barend.bentvelsen@vanoord.com',
+        keywords='web pyramid pylons',
+        packages=find_packages(),
+        include_package_data=True,
+        install_requires=requires,
+        entry_points={
+            'paste.app_factory': [
+                'main = tutorial:main',
+                ]
+            }
+        )
