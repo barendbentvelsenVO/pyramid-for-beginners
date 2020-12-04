c1cab8754e67aefd8dfc3431bfc91f13e5cbfcb8
barendbentvelsenVO <barend.bentvelsen@vanoord.com>
2020-12-04 11:26:41 +0100

    included databse

--- tutorial/routes.py
+++ tutorial/routes.py
@@ -2,4 +2,10 @@
 
 def includeme(config):
     """Include these routes within the application"""
-    config.addoure('hello', '/')
+    config.add_route('info', '/api/v1/')
+    config.add_route('register', '/api/v1/accounts')
+    config.add_route('profile_detail', '/api/v1/accounts/{username}')
+    config.add_route('login', '/api/v1/accounts/login')
+    config.add_route('logout', '/api/v1/accounts/logout')
+    config.add_route('tasks', '/api/v1/accounts/{username}/tasks')
+    config.add_route('task_detail', '/api/v1/accounts/{username}/tasks/{id}')
--- tutorial/views.py
+++ tutorial/views.py
@@ -1,8 +1,26 @@
 # tutorial/views.py
 
+from datetime import datetime
 from pyramid.view import view_config
+from todo.models import Task, User
 
-@view_config(route_name="hello", renderer="string")
-def hello_world(request):
-    """Print 'Hello, world!' as the response body."""
-    return 'Hello, world!'
+INCOMING_DATE_FMT = '%d/%m/%Y %H:%M:%S'
+
+@view_config(route_name="tasks", request_method="POST", renderer='json')
+def create_task(request):
+    """Create a task for one user."""
+    response = request.response
+    response.headers.extend({'Content-Type': 'application/json'})
+    user = request.dbsession.query(User).filter_by(username=request.matchdict['username']).first()
+    if user:
+        due_date = request.json['due_date']
+        task = Task(
+            name=request.json['name'],
+            note=request.json['note'],
+            due_date=datetime.strptime(due_date, INCOMING_DATE_FMT) if due_date else None,
+            completed=bool(request.json['completed']),
+            user_id=user.id
+        )
+        request.dbsession.add(task)
+        response.status_code = 201
+        return {'msg': 'posted'}
